a937eb214a59ff0edaa38835f62c7785fed34653
nikolouzos <nikos@genoch.com>
2025-02-27 18:30:10 +0000

    [feat] Theme and Color Updates
    Updated AccentColor values
    Changed ThemeBackground, ThemePrimary, and ThemeSecondary color values
    Added systemBackground and systemLabel colors
    Updated Colors+SwiftUI.swift

--- Projects/AppResources/Sources/Colors+SwiftUI.swift
+++ Projects/AppResources/Sources/Colors+SwiftUI.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 public extension SwiftUI.Color {
-    static let themeBackground = AppResourcesAsset.Colors.themeBackground.swiftUIColor
+    // MARK: - System
+    static let systemBackground = Color(uiColor: UIColor.systemBackground)
+    static let systemLabel = Color(uiColor: UIColor.label)
+    
+    // MARK: - Theme
+    static let tint = AppResourcesAsset.Colors.accentColor.swiftUIColor
+    static let themePrimary = AppResourcesAsset.Colors.themePrimary.swiftUIColor
     static let themeSecondary = AppResourcesAsset.Colors.themeSecondary.swiftUIColor
 }
