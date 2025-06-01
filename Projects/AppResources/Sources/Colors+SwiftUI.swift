627d2537925b9c5b2d0152983240f422e05dfc06
nikolouzos <nikos@genoch.com>
2025-06-01 19:18:15 +0100

    [feat] Update UI resources (colors, assets).
    - Introduces a new subscription management background pattern
    - Updates the app's color palette in Colors+SwiftUI.swift with a new `separator` color

--- Projects/AppResources/Sources/Colors+SwiftUI.swift
+++ Projects/AppResources/Sources/Colors+SwiftUI.swift
@@ -2,10 +2,13 @@ import SwiftUI
 
 public extension SwiftUI.Color {
     // MARK: - System
+
     static let systemBackground = Color(uiColor: UIColor.systemBackground)
     static let systemLabel = Color(uiColor: UIColor.label)
-    
+    static let separator: Color = .init(uiColor: UIColor.separator)
+
     // MARK: - Theme
+
     static let tint = AppResourcesAsset.Colors.accentColor.swiftUIColor
     static let themePrimary = AppResourcesAsset.Colors.themePrimary.swiftUIColor
     static let themeSecondary = AppResourcesAsset.Colors.themeSecondary.swiftUIColor
